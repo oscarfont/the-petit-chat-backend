7bf4520a68370ed415e1c2b6f931b0ee5354afa1
oscarfont <oscar.font01@alumni.upf.edu>
2025-05-05 23:42:01 +0200

    create sh to build lambda zip

---
+++ ai-service/package-and-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir output
+
+docker build -f lambda-builder.dockerfile -t lambda-builder .
+docker run --rm -v "$(pwd)"/output:/output lambda-builder
+
+chmod -R 777 output
+mv /home/ofont/Proyectos/petit-chat/the-petit-chat-backend/ai-service/output/ai-service.zip /home/ofont/Proyectos/petit-chat/the-petit-chat-backend/ai-service/ai-service.zip
+rm -r output
+
+zip -r ./ai-service.zip ./app -x "/app/*__pycache__/*"
